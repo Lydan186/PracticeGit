134dc122253be95163d0d178a6ac0655a89fd48b
Lydan186 <dilanchava48gmail.com>
2022-08-30 20:13:55 -0600

    Se agrego un nuveo proyecto

---
+++ src/practicegit/PracticeGit.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicegit;
+
+/**
+ *
+ * @author dilan
+ */
+public class PracticeGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola a todos");
+    }
+    
+}
